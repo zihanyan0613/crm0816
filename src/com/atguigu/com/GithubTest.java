9849ae4ecbe1a8790fde6a6c7a4f010c087da467
zihanyan0613 <import_java0613@126.com>
2016-01-07 00:20:21 +0800

    from github!
    from github!

--- src/com/atguigu/com/GithubTest.java
+++ src/com/atguigu/com/GithubTest.java
@@ -5,5 +5,6 @@ public class GithubTest {
 	public static void main(String[] args) {
 		System.out.println("hello Github!");
 		System.out.println("i am comming~");
+		System.out.println("from github!");
 	}
 }
