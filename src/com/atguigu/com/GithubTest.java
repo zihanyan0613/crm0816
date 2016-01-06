891f40867c0e366036c79d1710d082f684e8e8a7
yzh <import_java0613@126.com>
2016-01-07 00:33:26 +0800

    change  from local

--- src/com/atguigu/com/GithubTest.java
+++ src/com/atguigu/com/GithubTest.java
@@ -7,5 +7,6 @@ public class GithubTest {
 		System.out.println("i am comming~");
 		System.out.println("from github!");
 		System.out.println("change from github!");
+		System.out.println("change from local!");
 	}
 }
